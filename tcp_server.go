d7b483b1472df9c519c9b7936ba9f47b2267bb3e
Spankyduck <sejessen4@gmail.com>
2023-04-20 21:05:47 +0000

    endret file

--- tcp_server.go
+++ tcp_server.go
@@ -1,11 +1,11 @@
-dpackage main
+package main
 
 import (
 	"io"
 	"log"
 	"net"
 	"sync"
-        "
+
 )
 
 func main() {
@@ -47,12 +47,9 @@ func main() {
 						if err != io.EOF {
 							log.Println(err)
 						}
-						return // fra for løkke
-					}
+						return // fra for løkkeø
 
-                                        dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-                                        log.Println("Dekrypter melding: ", string(dekryptertMelding))
-                                        switch msg := string(dekrypterMelding) { ...
+                                       }
 				}
 			}(conn)
 		}
