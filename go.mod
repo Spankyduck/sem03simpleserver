57bb6875ae9fcff81968b6769e36502269c55921
Spankyduck <sejessen4@gmail.com>
2023-04-27 14:17:36 +0000

    lololol

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module tcp_server.go
 
 go 1.18
 
-require github.com/Spankyduck/is105sem03 v0.0.0-20230419090342-bbabb2540b34 // indirect
+require (
+	github.com/Spankyduck/funtemps v0.0.0-20230220150230-57334d79374a // indirect
+	github.com/Spankyduck/is105sem03 v0.0.0-20230419090342-bbabb2540b34 // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -1,62 +1,91 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
-	"net"
-	"sync"
+    "fmt"
+    "io"
+    "log"
+    "net"
+    "strconv"
+    "strings"
+    "sync"
 
-	"github.com/Spankyduck/is105sem03/mycrypt"
+    "github.com/Spankyduck/is105sem03/mycrypt"
+    "github.com/Spankyduck/funtemps/conv"
 )
 
+
 func main() {
+    var wg sync.WaitGroup
 
-	var wg sync.WaitGroup
+    server, err := net.Listen("tcp", "172.17.0.3:12345")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer server.Close()
 
-	server, err := net.Listen("tcp", "172.17.0.4:8050")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", server.Addr().String())
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			log.Println("f  r server.Accept() kallet")
-			conn, err := server.Accept()
-			if err != nil {
-				return
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for l  kke
-					}
-					switch msg := string(buf[:n]); msg {
-					case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
-						log.Println("Dekrypter melding: ", string(dekryptertMelding))
-						_, err = io.Copy(c, bytes.NewReader([]byte(string(dekryptertMelding))))
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for l  kke
-					}
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+    log.Printf("bundet til %s", server.Addr().String())
+
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+
+        for {
+            log.Println("før server.Accept() kallet")
+
+            conn, err := server.Accept()
+            if err != nil {
+                log.Println(err)
+                continue
+            }
+
+            wg.Add(1)
+            go func(c net.Conn) {
+                defer wg.Done()
+                defer conn.Close()
+
+                for {
+                    buf := make([]byte, 2048)
+                    n, err := c.Read(buf)
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return
+                    }
+
+
+                    dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                    log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+                    msgString := string(dekryptertMelding)
+
+                    switch msgString {
+                    case "ping":
+                        kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+                        log.Println("Kryptert melding: ", string(kryptertMelding))
+                        _, err = c.Write([]byte(string(kryptertMelding)))
+                    case "Kjevik;SN39040;18.03.2022 01:50;6":
+                        parts := strings.Split(msgString, ";")
+                        celsius, _ := strconv.ParseFloat(parts[1], 64)
+                        fahrenheit := conv.CelsiusToFahrenheit(celsius)
+                        kryptertMelding := mycrypt.Krypter([]rune(fmt.Sprintf("Temperaturen i Fahrenheit er %.2f", fahrenheit)), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                        log.Println("Kryptert melding: ", string(kryptertMelding))
+                        _, err = c.Write([]byte(string(kryptertMelding)))
+                    default:
+                        _, err = c.Write(buf[:n])
+                    }
+
+                    if err != nil {
+                        if err != io.EOF {
+                            log.Println(err)
+                        }
+                        return
+                    }
+                }
+            }(conn)
+        }
+    }()
+
+    wg.Wait()
 }
 
